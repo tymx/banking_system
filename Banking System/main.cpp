5631c9019c43feb68fe75a533e30a328f227e2b5
Taylor <taylorannmartinez16@gmail.com>
2017-07-26 17:37:34 -0400

    Account Function Done

--- Banking System/main.cpp
+++ Banking System/main.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <ostream>
+#include <vector>
+#include <string>
+#include <cctype>
 #include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
+struct account
+{
+    string firstName;
+    string lastName;
+    int accountId;
+    long long int phoneNumber;
+    string accountType;
+    int amount;
+};
+
+string ACCOUNTFILENAME = "accounts.txt";
+
 int welcomeScreen(int counter);
 void devScreen();
-void mainMenu();
+void mainMenu(vector<account>& acc);
+void selectMenu(int opt, vector<account>& acc);
+void getAccounts(vector<account>& acc);
+void openAccount(vector<account>& acc);
+void saveAccounts(vector<account>& acc);
+void despositAmount(vector<account>& acc);
+void withdrawAmount();
+void displayAccounts(vector<account>& acc);
+void displayTransactions();
+void checkStrings(string str);
 
 int main()
 {
+    vector<account> accounts;
+
     int option;
     int i = 0;
+
+    ifstream existFile(ACCOUNTFILENAME.c_str());
+    if(existFile)
+    {
+        getAccounts(accounts);
+    }
 
     do
     {
@@ -21,7 +56,7 @@ int main()
         }
         if((option == 2 && i == 0) || (option == 1 && i != 0))
         {
-            mainMenu();
+            mainMenu(accounts);
             option = 99;
         }
         i++;
@@ -56,7 +91,6 @@ int welcomeScreen(int counter)
 void devScreen()
 {
     system("cls");
-    //cout << "Press zero when you wish to go back to the Welcome Screen" << endl << endl;
     cout << "Developers:" << endl << endl;
     cout << "Angel Castro" << endl;
     cout << "Taylor Martinez" << endl << endl;
@@ -66,8 +100,10 @@ void devScreen()
     return;
 }
 
-void mainMenu()
+void mainMenu(vector<account>& acc)
 {
+    int option;
+
     system("cls");
     cout << "Main Menu: " << endl << endl;
 
@@ -84,5 +120,253 @@ void mainMenu()
 
     cout << endl;
 
+    cout << "Select an option: " << endl;
+    cin >> option;
+
+    selectMenu(option, acc);
+
     return;
 }
+
+void selectMenu(int opt, vector<account>& acc)
+{
+
+    if(opt == 1)
+    {
+        openAccount(acc);
+        saveAccounts(acc);
+    }
+    /*
+    if (opt == 2)
+    {
+        depositAmount();
+    }
+    if(opt == 3)
+    {
+        withdrawAmount();
+    }*/
+    if(opt == 4)
+    {
+        displayAccounts(acc);
+    }/*
+    if(opt == 5)
+    {
+        displayTransactions();
+    }*/
+
+    return;
+}
+
+void getAccounts(vector<account>& acc)
+{
+    ifstream inFile;
+    account tmp;
+    char ch;
+
+    inFile.open(ACCOUNTFILENAME.c_str());
+
+    while(!inFile.eof())
+    {
+        inFile >> tmp.accountId;
+        inFile >> tmp.accountType;
+        inFile >> tmp.firstName;
+        inFile >> tmp.lastName;
+        inFile >> tmp.phoneNumber;
+        inFile >> tmp.amount;
+        inFile.ignore(1000,'\n');
+        inFile.ignore(1000,'\n');
+
+        acc.push_back(tmp);
+    }
+
+    inFile.close();
+
+    return;
+}
+
+void openAccount(vector<account>& acc)
+{
+    account tmp;
+    string str;
+    bool check = false;
+    int counter = 1;
+    int num;
+    int numID = 0;
+
+    system("cls");
+
+    //First Name
+    cout << endl << "Enter first name: " << endl;
+    cin >> tmp.firstName;
+
+    checkStrings(tmp.firstName);
+
+    //Last Name
+    cout << "Enter last name: " << endl;
+    cin >> tmp.lastName;
+
+    checkStrings(tmp.lastName);
+
+    //Phone Number
+    cout << "Enter phone number: " << endl;
+    cin >> tmp.phoneNumber;
+
+    /*while(!cin.fail())
+    {
+        cin.clear();
+        cin.ignore(1000,'\n');
+        cout << "You cannot enter a character in this field. Enter numbers only" << endl;
+        cout << "Please enter information in field again" << endl;
+        cin.ignore(1000,'\n');
+        cin >> tmp.phoneNumber;
+        cin.ignore(1000,'\n');
+    }*/
+
+    check = false;
+
+    /*while(!check)
+    {
+
+        num = tmp.phoneNumber;
+
+        while(num >= 0)
+        {
+            num /= 10;
+            counter++;
+        }
+
+        if(counter != 10)
+        {
+            cout << "The number entered does not contain 10 digits" << endl;
+            cout << "Please enter a correct phone number" << endl;
+            cin >> tmp.phoneNumber;
+        }
+        else
+        {
+            check = true;
+        }
+
+        counter = 1;
+
+    }*/
+
+    //Account ID
+
+    tmp.accountId = acc.size() + 1;
+
+    cout << "Account ID: " << setfill('0') << setw(10) << tmp.accountId << endl;
+
+    //Account Type
+    cout << "Please choose the number of the type of account you want: " << endl;
+    cout << "1: Checking" << endl;
+    cout << "2: Saving" << endl;
+    cin >> num;
+
+    if(num == 1)
+    {
+        tmp.accountType = "Checking";
+    }
+    if(num == 2)
+    {
+        tmp.accountType = "Saving";
+    }
+
+    //Amount
+    cout << "Please input your initial desposit: " << endl;
+    cin >> tmp.amount;
+
+    acc.push_back(tmp);
+
+    cout << endl << "Account has been made" << endl;
+
+    cout << "Press 0 if you wish return to the main menu" << endl;
+    cin >> num;
+
+    if(num == 0)
+    {
+        mainMenu(acc);
+    }
+
+    return;
+
+}
+
+void saveAccounts(vector<account>& acc)
+{
+    ofstream outFile;
+
+    account tmp;
+
+    outFile.open(ACCOUNTFILENAME.c_str());
+
+    for(int i = 0; i < acc.size(); ++i)
+    {
+        outFile << acc.at(i).accountId << " "
+        << acc.at(i).accountType << " "
+        << acc.at(i).firstName << " "
+        << acc.at(i).lastName << " "
+        << acc.at(i).phoneNumber << " "
+        << acc.at(i).amount << endl;
+    }
+
+    outFile.close();
+
+    return;
+}
+
+void despositAmount(vector<account>& acc)
+{
+    system("cls");
+}
+
+void displayAccounts(vector<account>& acc)
+{
+
+    system("cls");
+    cout << endl;
+    cout << "************************************************************************************************************************" << endl;
+    cout << "                                                    Accounts                                                      " << endl;
+    cout << "************************************************************************************************************************" << endl;
+    cout << endl;
+
+    for(int i = 0; i < acc.size(); ++i)
+    {
+        cout << acc.at(i).accountId << " "
+        << acc.at(i).accountType << " "
+        << acc.at(i).firstName << " "
+        << acc.at(i).lastName << " "
+        << acc.at(i).phoneNumber << " "
+        << acc.at(i).amount <<  endl;
+    }
+}
+
+//Program by Taylor Martinez. Checks to see if a string has any numbers within
+void checkStrings(string str)
+{
+    bool check = false;
+    string checkstr;
+
+    checkstr = str;
+
+    while (!check)
+    {
+        for(unsigned int i = 0; i < checkstr.length(); ++i)
+        {
+            if(isdigit(checkstr.at(i)))
+            {
+                cout << "You cannot enter a number in this field. Enter characters only" << endl;
+                cout << "Please enter information in field again" << endl;
+                cin >> checkstr;
+            }
+            else
+            {
+                check = true;
+                str = checkstr;
+            }
+
+        }
+    }
+
+    return;
+}
+
